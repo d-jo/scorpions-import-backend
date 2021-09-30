fc6a20474bc68ca241bc207495a4ce7344cebbb2
d-jo <declan.l.johnson@gmail.com>
2021-09-30 14:35:17 -0500

    Update parsing file

--- document_processing.py
+++ document_processing.py
@@ -1,51 +1,51 @@
 # %%
-from docx import Document
-from docx.oxml.ns import qn
-from lxml import etree
-
-
-def is_checked(docElements):
-    """Not able to detemine if checkbox is checked or not."""
-    for docElem in docElements:
-        checkboxes = etree.ElementBase.xpath(docElem._element, './/w14:checkbox', namespaces=docElem._element.nsmap)
-        for checkbox in checkboxes:
-            p = docElem._element
-            print(checkbox)
-            print(p.xml)
-            for item in checkbox.findall('.//w14:checkbox', namespaces=docElem._element.nsmap):
-                print(item)
-                print(etree.dump(item))
-
-
-def read_document(document):
-    """Read in document and print out the paragraph contents."""
-    documentText = []
-    documentElements = []
-    for paragraph in document.paragraphs:
-        if paragraph.text == '':
-            continue
-        documentText.append(paragraph.text)
-        documentElements.append(paragraph)
-        print(paragraph)
-    print('\n'.join(documentText))
-    # is_checked(documentElements)
-
-
-f = open('./data/grad2018-regular.docx', 'rb')
-document = Document(f)
-read_document(document)
-# %%
-
-# w:tc
-#   w:p
-#       w:sdt -> w:sdtPr
-#           w14:checkbox -> w14:checked
-#       w:r
-#           w:t = text
-#           
-#      
-
-# %%
+#from docx import Document
+#from docx.oxml.ns import qn
+#from lxml import etree
+#
+#
+#def is_checked(docElements):
+#    """Not able to detemine if checkbox is checked or not."""
+#    for docElem in docElements:
+#        checkboxes = etree.ElementBase.xpath(docElem._element, './/w14:checkbox', namespaces=docElem._element.nsmap)
+#        for checkbox in checkboxes:
+#            p = docElem._element
+#            print(checkbox)
+#            print(p.xml)
+#            for item in checkbox.findall('.//w14:checkbox', namespaces=docElem._element.nsmap):
+#                print(item)
+#                print(etree.dump(item))
+#
+#
+#def read_document(document):
+#    """Read in document and print out the paragraph contents."""
+#    documentText = []
+#    documentElements = []
+#    for paragraph in document.paragraphs:
+#        if paragraph.text == '':
+#            continue
+#        documentText.append(paragraph.text)
+#        documentElements.append(paragraph)
+#        print(paragraph)
+#    print('\n'.join(documentText))
+#    # is_checked(documentElements)
+#
+#
+#f = open('./data/grad2018-regular.docx', 'rb')
+#document = Document(f)
+#read_document(document)
+## %%
+#
+## w:tc
+##   w:p
+##       w:sdt -> w:sdtPr
+##           w14:checkbox -> w14:checked
+##       w:r
+##           w:t = text
+##           
+##      
+#
+## %%
 
 import xml.etree.ElementTree as ET
 
@@ -68,4 +68,8 @@ def rec_traverse(tr):
         result.extend(rec_traverse(c))
     
     return result
+
+
+for a in rec_traverse(tree.getroot()):
+    print(a)
 # %%
