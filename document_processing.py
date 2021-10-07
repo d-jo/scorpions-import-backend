e66a015dc82ad824e3a8b4c20d83cc6e5abf0798
Christian Reza <creza@unomaha.edu>
2021-10-07 17:30:41 -0500

    clean up initial pass

--- document_processing.py
+++ document_processing.py
@@ -2,18 +2,48 @@
 from docx import Document
 from lxml import etree
 import xml.etree.ElementTree as ET
+import pandas as pd
+
+def pandas_table(document, table_num=1, nheader=1):
+    """
+    Attempt at printing tables in a easy-to-read format
+    https://medium.com/@karthikeyan.eaganathan/read-tables-from-docx-file-to-pandas-dataframes-f7e409401370
+
+    :param document: opened document from python-docx.
+    :param table_num: table number in document.
+    :param nheader: number of headers in table.
+    """
+    table = document.tables[table_num-1]
+    data = [[cell.text for cell in row.cells] for row in table.rows]
+    df = pd.DataFrame(data)
+    if nheader == 1:
+        df = df.rename(columns=df.iloc[0]).drop(df.index[0]).reset_index(drop=True)
+    elif nheader == 2:
+        outside_col, inside_col = df.iloc[0], df.iloc[1]
+        hier_index = pd.MultiIndex.from_tuples(list(zip(outside_col, inside_col)))
+        df = pd.DataFrame(data, columns=hier_index).drop(df.index[[0,1]]).reset_index(drop=True)
+    elif nheader > 2:
+        print("more than two headers not supported")
+        df = pd.DataFrame()
+    print(df)
 
 
 def rec_traverse(tr):
+    """
+    Recusively go through xml tags to find checkboxes and their value.
+    :param tr: tag element.
+    """
     result = []
     if tr.tag.endswith("t"):
-        result.append((tr.tag, tr.text))
+        result.append((tr.text.strip()))
         pass
     if tr.tag.endswith("checked"):
         for attr in tr.attrib:
             if attr.endswith("val"):
                 checked = tr.attrib[attr]
-                result.append(("checkbox", checked))
+                # Only add the ones that are checked, other are assumed not checked by default
+                if checked == '1':
+                    result.append(("checkbox", checked))
                 
     #if tr.tag.endswith()
     for c in tr:
@@ -23,26 +53,25 @@ def rec_traverse(tr):
 
 
 def is_checked(docElements):
-    """Not able to detemine if checkbox is checked or not."""
+    """
+    Going through paragraph elements, determine if contents
+    has a checkbox, and if it is checked or not.
+
+    :param docElements: array of paragraph elements from python-docx.
+    """
     for docElem in docElements:
-        checkboxes = etree.ElementBase.xpath(docElem._element, './/w14:checkbox', namespaces=docElem._element.nsmap)
-        # for checkbox in checkboxes:
-        #     print(checkbox)
+        # checkboxes = etree.ElementBase.xpath(docElem._element, './/w14:checkbox', namespaces=docElem._element.nsmap)
         p = docElem._element
         tree=ET.fromstring(p.xml)
-        # print(tree)
         for a in rec_traverse(tree):
-            print('****************************************************\n')
             print(a)
-            # print(checkbox)
-            # print(p.xml)
-            # for item in checkbox.findall('.//w14:checkbox', namespaces=docElem._element.nsmap):
-                # print(item)
-                # print(etree.dump(item))
 
 
 def read_document(document):
-    """Read in document and print out the paragraph contents."""
+    """
+    Read in document and print out the paragraph contents.
+    :param document: opened document from python-docx.
+    """
     documentText = []
     documentElements = []
     for paragraph in document.paragraphs:
@@ -50,8 +79,7 @@ def read_document(document):
             continue
         documentText.append(paragraph.text)
         documentElements.append(paragraph)
-        # print(paragraph)
-    tablesCells = []
+    tableParas = []
     tablesCellsText = []
     for table in document.tables:
         for row in table.rows:
@@ -59,7 +87,7 @@ def read_document(document):
                 for para in cell.paragraphs:
                     if para.text == '':
                         continue
-                    tablesCells.append(para)
+                    tableParas.append(para)
                     tablesCellsText.append(para.text.strip())
     print('Document plain text\n')
     print('****************************************************\n')
@@ -71,8 +99,10 @@ def read_document(document):
     print('****************************************************\n')
     print('\n'.join(tablesCellsText))
     print('****************************************************\n')
-    # is_checked(documentElements)
-    is_checked(tablesCells)
+    
+    is_checked(tableParas)
+
+    # pandas_table(document)
 
 
 f = open('./data/grad2018-regular.docx', 'rb')
@@ -88,13 +118,3 @@ read_document(document)
 #           w:t = text
 #           
 #      
-
-# %%
-
-import xml.etree.ElementTree as ET
-
-
-tree=ET.parse('data/test/word/document.xml')
-
-
-# %%
