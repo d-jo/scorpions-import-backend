035c832ad5c23818a543d6013ae3805936dd5020
Christian Reza <creza@unomaha.edu>
2021-11-03 15:08:01 -0500

    able to upload multiple files

--- webapi/files/files.py
+++ webapi/files/files.py
@@ -25,16 +25,14 @@ def upload_file():
       flash('no file part')
       return redirect(request.url)
 
-    file = request.files['file']
-
-    if file.filename == "":
-      flash('no selected file')
-      return redirect(request.url)
-
-    if file and is_allowed_ext(file.filename):
-      filename = secure_filename(file.filename)
-      file.save(os.path.join(current_app.config['UPLOAD_FOLDER'], filename))
-      return redirect(request.url)
+    uploadedFiles = request.files.getlist("file")
+    for file in uploadedFiles:
+      if file.filename == "":
+        flash('no selected file')
+      if file and is_allowed_ext(file.filename):
+        filename = secure_filename(file.filename)
+        file.save(os.path.join(current_app.config['UPLOAD_FOLDER'], filename))
+    return redirect(request.url)
 
   return """
     <!doctype html>
