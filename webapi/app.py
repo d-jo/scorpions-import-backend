4543080f4ae21e343c297f858e0359d0c0117936
ulahvinovich <ulahvinovich@unomaha.edu>
2021-11-10 21:10:46 -0600

    autho changes

--- webapi/app.py
+++ webapi/app.py
@@ -1,6 +1,6 @@
 import os, glob
 import errno
-from flask import Flask, flash, request, redirect, url_for
+from flask import Flask, flash, request, jsonify,redirect, url_for
 from werkzeug.utils import secure_filename
 from files.files import files_bp, views_bp, dashboard_bp
 from reports.reports import reports_bp
@@ -8,11 +8,34 @@ from database.driver import db_init
 import json
 from audits.audits import audit_bp
 from statistics.statistics import statistic_bp
-
+from flask_cors import cross_origin
+from auth.auth import requires_auth, AuthError, requires_scope
 TARGET_FOLDER = './data'
 
 app = Flask(__name__)
 
+@app.route("/api/private-scoped")
+@cross_origin(headers=["Content-Type", "Authorization"])
+@cross_origin(headers=["Access-Control-Allow-Origin", "http://localhost:3000"])
+@requires_auth
+def private_scoped():
+    """A valid access token and an appropriate scope are required to access this route
+    """
+    if requires_scope("read:messages"):
+        response = "You need to be authenticated and have a scope of read:messages to see this."
+        return jsonify(message=response)
+    raise AuthError({
+        "code": "Unauthorized",
+        "description": "You don't have access to this resource"
+    }, 403)
+   
+
+@app.errorhandler(AuthError)
+def handle_auth_error(ex):
+    response = jsonify(ex.error)
+    response.status_code = ex.status_code
+    return response
+ 
 # ====== LOAD CONFIG ====== 
 with open('config.json') as config_file:
     config = json.load(config_file)
