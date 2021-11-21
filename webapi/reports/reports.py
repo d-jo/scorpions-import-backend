186e016f05ca8fe2607bf3462daa7aa9931d291c
d-jo <declan.l.johnson@gmail.com>
2021-11-21 11:10:05 -0600

    remove trigger_process endpoint

--- webapi/reports/reports.py
+++ webapi/reports/reports.py
@@ -12,19 +12,6 @@ from auth.auth import requires_auth, get_token_auth_header
 
 reports_bp = Blueprint("reports_bp", __name__)
 
-@reports_bp.route('/trigger_process', methods=['GET'])
-@requires_auth
-def trigger_process_files():
-  for f in glob.glob(os.path.join(current_app.config['UPLOAD_FOLDER'], '*.txt')):
-    with open(f, 'r') as file:
-      # TODO change this to use the document parsing funciton
-      # python is sometimes hard to work with multiple files unless the project
-      # is set up a current way so we will deal with that later
-      print("=====")
-      print(file.read())
-      print("=====")
-  return { "message":"success" }
-
 
 @reports_bp.route('/<file_id>/edit', methods=['POST'])
 @requires_auth
